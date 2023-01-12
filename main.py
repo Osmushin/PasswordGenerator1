6a9cc799372011deab2b8a1a5c68957c5932300d
Osmushin <toxa_os@mail.ru>
2023-01-12 17:51:57 +0300

    First commit (no working version)

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from random import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+chars_list = ''
+
+print('Добро пожаловать в программу генерации безопасных паролей!')
+print('Прежде чем я сгенерирую Вам самые лучшие пароли ответьте на несколько вопросов')
+
+print('Введите необходимое количество паролей (это должно быть положительное число)')
+amount = input()
+print('Введите необходимое количество символов в одном пароле (это должно быть положительное число)')
+long_digit = input()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def if_yesorno(answer, chars_list, spis):
+    if answer in 'даДаДАнетНетНЕТ':
+        if answer in 'даДаДА':
+            chars_list += spis
+            return True, print(chars_list)
+        elif answer in 'нетНетНЕТ':
+            return True, print('В вашем пароле не будут использованы цифры')
+    else:
+        return False
+
+    print('Включать ли в состав пароля цифры? (ответьте "да" или "нет"')
+    answerdigit = input()
+    if if_yesorno(answerdigit, chars_list, digits) != True:
+        answerdigit = input()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print('Включать ли в состав пароля маленькие буквы? (ответьте "да" или "нет"')
+lowerletters = input()
+print('Включать ли в состав пароля большие буквы? (ответьте "да" или "нет"')
+upperletters = input()
+print('Включать ли в состав пароля символы? (ответьте "да" или "нет"')
+simbols = input()
+print('Включать ли в состав пароля неоднозначные символы? (ответьте "да" или "нет"')
+sim_simbols = input()
