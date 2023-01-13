ee29f354cbf9b784a1e9c5e08e2f6fe0f2da080e
Osmushin <toxa_os@mail.ru>
2023-01-13 18:32:44 +0300

    The second commit (edit function is_digit)

--- main.py
+++ main.py
@@ -6,31 +6,38 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 chars_list = ''
 
-print('Добро пожаловать в программу генерации безопасных паролей!')
-print('Прежде чем я сгенерирую Вам самые лучшие пароли ответьте на несколько вопросов')
 
-print('Введите необходимое количество паролей (это должно быть положительное число)')
-amount = input()
-print('Введите необходимое количество символов в одном пароле (это должно быть положительное число)')
-long_digit = input()
-
-
-def if_yesorno(answer, chars_list, spis):
-    if answer in 'даДаДАнетНетНЕТ':
-        if answer in 'даДаДА':
-            chars_list += spis
-            return True, print(chars_list)
-        elif answer in 'нетНетНЕТ':
-            return True, print('В вашем пароле не будут использованы цифры')
+# Функция проверки на число
+def is_digit(digit):
+    if digit.isdigit():
+        if int(digit) >= 1:
+            return True
+        else:
+            return False
     else:
         return False
 
-    print('Включать ли в состав пароля цифры? (ответьте "да" или "нет"')
-    answerdigit = input()
-    if if_yesorno(answerdigit, chars_list, digits) != True:
-        answerdigit = input()
 
 
+print('Добро пожаловать в программу генерации безопасных паролей!')
+print('Прежде чем я сгенерирую Вам самые лучшие пароли ответьте на несколько вопросов')
+
+print('Введите необходимое количество паролей (это должно быть целое положительное число)')
+amount = input()
+while not is_digit(amount):
+    print('Это должно быть целое положительное число')
+    amount = input()
+
+print('Введите необходимое количество символов в одном пароле (это должно быть положительное число)')
+long_digit = input()
+while not is_digit(long_digit):
+    print('Это должно быть целое положительное число')
+    long_digit = input()
+
+
+
+print('Включать ли в состав пароля цифры? (ответьте "да" или "нет"')
+answerdigit = input()
 
 
 print('Включать ли в состав пароля маленькие буквы? (ответьте "да" или "нет"')
