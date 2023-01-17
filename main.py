c4ac429564eaf47357a78060884182fc486dd82a
Osmushin <toxa_os@mail.ru>
2023-01-17 14:00:16 +0300

    Password Generator V1.0 - good work version

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ digits = '0123456789'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
+noneonesimbols = 'il1Lo0O'
 chars = []
 
 
@@ -18,19 +19,28 @@ def is_digit(digit):
         return False
 
 
-#Функция проверки ответа на да и нет
+# Функция проверки ответа на да и нет
 def word_yesorno(answer):
     if answer in 'даДаДАнетНетНЕТ':
         return True
     else:
         return False
 
-#Функция проверки Да или Нет:
+
+# Функция проверки Да или Нет и внесение симолов в массив chars
 def yesorno(answer, simbols):
     if answer in 'даДаДА':
         return chars.extend(simbols)
     elif answer in 'нетНетНЕТ':
         return False
+
+
+# Функция генерации паролей
+def generate_password(length, chars):
+    password = []
+    for i in range(1, length + 1):
+        password.append(chars[randint(0, len(chars))])
+    return password
 
 
 print('Добро пожаловать в программу генерации безопасных паролей!')
@@ -48,31 +58,45 @@ while not is_digit(long_digit):
     print('Это должно быть целое положительное число')
     long_digit = input()
 
-
-
 print('Включать ли в состав пароля цифры? (ответьте "да" или "нет")')
 answerdigit = input()
 while not word_yesorno(answerdigit):
     print('ответьте "да" или "нет"')
     answerdigit = input()
 yesorno(answerdigit, digits)
-print(chars)
-
-
 
 print('Включать ли в состав пароля маленькие буквы? (ответьте "да" или "нет")')
 lowerletters = input()
 while not word_yesorno(lowerletters):
     print('ответьте "да" или "нет"')
     lowerletters = input()
-if yesorno(lowerletters) == True:
-    chars.extend(lowercase_letters)
-print(chars)
+yesorno(lowerletters, lowercase_letters)
 
-
-print('Включать ли в состав пароля большие буквы? (ответьте "да" или "нет"')
+print('Включать ли в состав пароля большие буквы? (ответьте "да" или "нет")')
 upperletters = input()
-print('Включать ли в состав пароля символы? (ответьте "да" или "нет"')
+while not word_yesorno(upperletters):
+    print('ответьте "да" или "нет"')
+    lowerletters = input()
+yesorno(upperletters, uppercase_letters)
+
+print('Включать ли в состав пароля символы? (ответьте "да" или "нет")')
 simbols = input()
-print('Включать ли в состав пароля неоднозначные символы? (ответьте "да" или "нет"')
+while not word_yesorno(simbols):
+    print('ответьте "да" или "нет"')
+    simbols = input()
+yesorno(simbols, punctuation)
+
+print('Включать ли в состав пароля неоднозначные символы (il1Lo0O)? (ответьте "да" или "нет")')
 sim_simbols = input()
+while not word_yesorno(sim_simbols):
+    print('ответьте "да" или "нет"')
+    sim_simbols = input()
+if sim_simbols in "нетНетНЕТ":
+    for i in range(len(noneonesimbols)):
+        chars.remove(noneonesimbols[i])
+
+for i in range(1, int(amount)+1):
+    print('Пароль №', i, ':', end='')
+    print(*generate_password(int(long_digit), chars), sep='')
+
+print('Спасибо за использование нашего генератора паролей!')
